0af93f5465252bc33a814c1282de159c3aebe08a
岩田康平 <iwater0715@gmail.com>
2020-09-24 20:21:35 +0900

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: ['./components/**/*.jsx', './pages/**/*.jsx'],
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
